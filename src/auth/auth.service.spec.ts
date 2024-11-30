7865f1e6d06847759a933dbcbe5525569171b6bd
Supawite Kiatrattanakorn <supawite08@gmail.com>
2024-11-30 13:44:58 +0700

    Update test for jwt sign and verification type of access or refresh token

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { JwtService, TokenExpiredError } from '@nestjs/jwt';
+import { RpcException } from '@nestjs/microservices';
+import { InternalServerErrorException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JWTServiceMock } from './mocks/jwt.service.mock';
 
@@ -7,9 +9,14 @@ describe('AuthService', () => {
   let jwtServiceMock: JWTServiceMock;
   let service: AuthService;
   let jwtService: JwtService;
+  const jwtAccessSecert = 'accessSecret';
+  const jwtRefreshSecert = 'refreshSecret';
 
   beforeEach(async () => {
-    jwtServiceMock = new JWTServiceMock();
+    jwtServiceMock = new JWTServiceMock({
+      accessSecret: jwtAccessSecert,
+      refreshSecret: jwtRefreshSecert,
+    });
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService],
@@ -24,6 +31,9 @@ describe('AuthService', () => {
 
     service = module.get<AuthService>(AuthService);
     jwtService = module.get<JwtService>(JwtService);
+
+    process.env.JWT_ACCESS_SECRET = jwtAccessSecert;
+    process.env.JWT_REFRESH_SECRET = jwtRefreshSecert;
   });
 
   it('should be defined', () => {
@@ -31,32 +41,95 @@ describe('AuthService', () => {
   });
 
   describe('signToken', () => {
-    it('should return token', async () => {
+    it('should return access token', async () => {
       // Arrange
       const userId = 1;
       const username = 'username';
-      jest.spyOn(jwtService, 'sign').mockReturnValue('jwttoken');
+      const type = 'access';
 
       // Act
-      const token = await service.signToken(userId, username);
+      const token = await service.signToken(userId, username, type);
 
       // Assert
-      expect(token).toBe('jwttoken');
+      expect(token).toEqual({
+        user: { sub: userId, username: username },
+        token: 'accessToken',
+      });
+    });
+
+    it('should return refresh token', async () => {
+      // Arrange
+      const userId = 1;
+      const username = 'username';
+      const type = 'refresh';
+
+      // Act
+      const token = await service.signToken(userId, username, type);
+
+      // Assert
+      expect(token).toEqual({
+        user: { sub: userId, username: username },
+        token: 'refreshToken',
+      });
+    });
+
+    it('should throw error if type is invalid', async () => {
+      // Arrange
+      const userId = 1;
+      const username = 'username';
+      const type = 'normal';
+
+      // Act & Assert
+      await expect(service.signToken(userId, username, type)).rejects.toThrow(
+        new RpcException(
+          new InternalServerErrorException(
+            `Invalid token type. Expected 'access' or 'refresh' (Got ${type})`,
+          ),
+        ),
+      );
     });
   });
 
   describe('verifyToken', () => {
-    it('should return payload', async () => {
+    it('should verify access token and return payload', async () => {
       // Arrange
-      const token = 'token';
+      const token = 'accessToken';
       const output = { sub: 1, username: 'username' };
-      jest.spyOn(jwtService, 'verify').mockReturnValue(output);
+      const type = 'access';
 
       // Act
-      const payload = await service.verifyToken(token);
+      const payload = await service.verifyToken(token, type);
 
       // Assert
-      expect(payload).toBe(output);
+      expect(payload).toEqual(output);
+    });
+
+    it('should verify refresh token and return payload', async () => {
+      // Arrange
+      const token = 'refreshToken';
+      const output = { sub: 2, username: 'username2' };
+      const type = 'refresh';
+
+      // Act
+      const payload = await service.verifyToken(token, type);
+
+      // Assert
+      expect(payload).toEqual(output);
+    });
+
+    it('should throw internal server error if type is invalid', async () => {
+      // Arrange
+      const token = 'accessToken';
+      const type = 'normal';
+
+      // Act & Assert
+      await expect(service.verifyToken(token, type)).rejects.toThrow(
+        new RpcException(
+          new InternalServerErrorException(
+            `Invalid token type. Expected 'access' or 'refresh' (Got ${type})`,
+          ),
+        ),
+      );
     });
 
     it('should throw error if token is invalid', async () => {
@@ -67,7 +140,7 @@ describe('AuthService', () => {
       });
 
       // Act & Assert
-      await expect(service.verifyToken(token)).rejects.toThrow(
+      await expect(service.verifyToken(token, 'access')).rejects.toThrow(
         new TokenExpiredError('token is invalid', null),
       );
     });
--- src/auth/mocks/jwt.service.mock.ts
+++ src/auth/mocks/jwt.service.mock.ts
@@ -1,16 +1,51 @@
+import { TokenExpiredError } from '@nestjs/jwt';
+
 export class JWTServiceMock {
   stubSign: any;
   stubVerify: any;
+  accessSecret: string;
+  refreshSecret: string;
 
-  constructor(stubSign = {}, stubVerify = {}) {
-    this.stubSign = stubSign;
+  constructor(secert, stubVerify = {}) {
+    this.accessSecret = secert?.accessSecret;
+    this.refreshSecret = secert?.refreshSecret;
     this.stubVerify = stubVerify;
   }
 
   build() {
     return {
-      sign: jest.fn().mockReturnValue(this.stubSign),
-      verify: jest.fn().mockReturnValue(this.stubVerify),
+      sign: jest
+        .fn()
+        .mockImplementation((payload, options) => this.sign(options)),
+      verify: jest
+        .fn()
+        .mockImplementation((token, options) => this.verify(token, options)),
     };
+  }
+
+  private sign(options: { secret: string }) {
+    switch (options.secret) {
+      case this.accessSecret:
+        return 'accessToken';
+      case this.refreshSecret:
+        return 'refreshToken';
+      default:
+        return 'invalidToken';
+    }
+  }
+
+  private verify(token: string, options: { secret: string }) {
+    switch (options.secret) {
+      case this.accessSecret:
+        return token === 'accessToken'
+          ? { sub: 1, username: 'username' }
+          : null;
+      case this.refreshSecret:
+        return token === 'refreshToken'
+          ? { sub: 2, username: 'username2' }
+          : null;
+      default:
+        throw new TokenExpiredError('token is invalid', null);
+    }
   }
 }
